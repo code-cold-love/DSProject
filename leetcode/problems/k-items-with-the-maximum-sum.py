71fd04b85f285ffcb87ff7c7a450a47e1427a97c
wenyuyan <wenyu.yan@miotech.com>
2023-03-29 17:22:10 +0800

    2600. K 件物品的最大和

---
+++ leetcode/problems/k-items-with-the-maximum-sum.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# 2600. K 件物品的最大和 https://leetcode.cn/problems/k-items-with-the-maximum-sum/
+class Solution:
+    def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
+        if k <= numOnes + numZeros:
+            return min(k, numOnes)
+        else:
+            return numOnes - (k - numOnes - numZeros)
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    print(obj.kItemsWithMaximumSum(3, 2, 0, 2))  # 2
+    print(obj.kItemsWithMaximumSum(3, 2, 0, 4))  # 3
+    print(obj.kItemsWithMaximumSum(6, 6, 6, 13))  # 5
