34e06ab7a89b6526e394a0d78d76a55abe2eea75
wenyuyan <wenyu.yan@miotech.com>
2023-03-30 14:15:34 +0800

    2602. 使数组元素全部相等的最少操作次数

--- leetcode/problems/minimum-operations-to-make-all-array-elements-equal.py
+++ leetcode/problems/minimum-operations-to-make-all-array-elements-equal.py
@@ -11,14 +11,14 @@ class Solution:
         # m == queries.length
         # 1 <= n, m <= 105
         # 1 <= nums[i], queries[i] <= 10**9
-        nums.sort()
+        nums.sort()  # 排序
         n = len(nums)
         acc = [0] + list(accumulate(nums))  # 前缀和数组
         ans = []
         for q in queries:
             i = bisect_left(nums, q)
-            left = q * i - acc[i]
-            right = acc[n] - acc[i] - q * (n - i)
+            left = q * i - acc[i]  # 左边的小于 q
+            right = acc[n] - acc[i] - q * (n - i)  # 右边的大于 q
             ans.append(left + right)
         return ans
 
@@ -27,5 +27,3 @@ if __name__ == '__main__':
     obj = Solution()
     print(obj.minOperations([2, 9, 6, 3], [10]))  # [20]
     print(obj.minOperations([3, 1, 6, 8], [1, 5]))  # [14, 10]
-    # (3+2) + (1+4) + (6-1) + (8-3) = 5 * 4
-    # (3+1+6+8) + (2+4-1-3) = 5 * 4
