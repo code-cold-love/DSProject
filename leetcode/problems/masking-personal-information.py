b5558e86aa844a9b158c7f95d664bfd1c7036095
wenyuyan <wenyu.yan@miotech.com>
2023-04-01 08:49:59 +0800

    831. 隐藏个人信息

---
+++ leetcode/problems/masking-personal-information.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# 831. 隐藏个人信息 https://leetcode.cn/problems/masking-personal-information/
+import re
+
+
+class Solution:
+    def maskPII(self, s: str) -> str:
+        if '@' in s:
+            first = s[0].lower()
+            last = re.search(r'\w+?(\w)@.+?$', s).group(1).lower()
+            suffix = re.search(r'^\w+?@(.+?)$', s).group(1).lower()
+            return first + '*****' + last + '@' + suffix
+        else:
+            s = re.sub(r'[\s()+-]', '', s)
+            local = s[-4:]
+            country = s[-13:-10]
+            return '+' + len(country) * '*' + '-***-***-' + local if country else '***-***-' + local
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    print(obj.maskPII('AB@qq.com'))  # a*****b@qq.com
+    print(obj.maskPII('1(234)567-890'))  # ***-***-7890
+    print(obj.maskPII('86-(10)12345678'))  # +**-***-***-5678
