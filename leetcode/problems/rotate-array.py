0db1929e1a42b4494e5a1a364c8230487b6cfa70
wenyuyan <wenyu.yan@miotech.com>
2023-04-06 13:38:11 +0800

    189. 轮转数组

---
+++ leetcode/problems/rotate-array.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# 189. 轮转数组 https://leetcode.cn/problems/rotate-array/
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k = k % n  # k < n
+        nums[:] = nums[n - k:] + nums[:n - k]
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    arr = [1, 2, 3, 4, 5, 6, 7]
+    obj.rotate(arr, k=3)
+    print(arr)  # [5, 6, 7, 1, 2, 3, 4]
