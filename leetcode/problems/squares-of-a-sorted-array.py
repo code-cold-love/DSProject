fdf5e14e48cccbf8ae5a332e73ac32349aca3241
wenyuyan <wenyu.yan@miotech.com>
2023-04-06 11:48:56 +0800

    977. 有序数组的平方

---
+++ leetcode/problems/squares-of-a-sorted-array.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# 977. 有序数组的平方 https://leetcode.cn/problems/squares-of-a-sorted-array/
+from typing import List
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        return sorted(i * i for i in nums)
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    print(obj.sortedSquares([-4, -1, 0, 3, 10]))  # [0, 1, 9, 16, 100]
