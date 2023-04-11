41d667022cb23346f77738ac98ee1a67ce79722a
wenyuyan <wenyu.yan@miotech.com>
2023-04-11 13:53:09 +0800

    1041. 困于环中的机器人

--- leetcode/problems/robot-bounded-in-circle.py
+++ leetcode/problems/robot-bounded-in-circle.py
@@ -4,18 +4,18 @@ class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
         direction = [[0, 1], [1, 0], [0, -1], [-1, 0]]
         idx = 0
-        x, y = 0, 0
+        pos = [0, 0]
         for instruction in instructions:
             if instruction == 'G':
-                x += direction[idx][0]
-                y += direction[idx][1]
+                pos[0] += direction[idx][0]
+                pos[1] += direction[idx][1]
             elif instruction == 'L':
                 idx -= 1
                 idx %= 4
             else:
                 idx += 1
                 idx %= 4
-        return idx != 0 or (x == 0 and y == 0)
+        return idx != 0 or (pos[0] == 0 and pos[1] == 0)
 
 
 if __name__ == '__main__':
