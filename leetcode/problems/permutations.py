b2bf70d6b5cdbc3ebf8f4b3ced6fc89823abdde0
code-cold-love <1193898262@qq.com>
2023-04-15 17:29:03 +0800

    46. 全排列

---
+++ leetcode/problems/permutations.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# 46. 全排列 https://leetcode.cn/problems/permutations/
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        ans = []
+
+        def backtrack(first: int = 0):
+            if first == n:  # 所有数均填完
+                ans.append(nums[:])
+            for i in range(first, n):
+                nums[first], nums[i] = nums[i], nums[first]  # 交换维护数组
+                backtrack(first + 1)  # 继续递归地填下一个数
+                nums[first], nums[i] = nums[i], nums[first]  # 撤销操作
+
+        backtrack()
+        return ans
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    print(obj.permute([1, 2, 3]))  # [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 2, 1], [3, 1, 2]]
