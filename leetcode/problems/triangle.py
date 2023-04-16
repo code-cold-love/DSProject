ec04fa6900db9b15608b256ff52bf75c42adfc37
code-cold-love <1193898262@qq.com>
2023-04-16 18:26:09 +0800

    120. 三角形最小路径和

---
+++ leetcode/problems/triangle.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# 120. 三角形最小路径和 https://leetcode.cn/problems/triangle/
+from typing import List
+
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp = [0] * n
+        dp[0] = triangle[0][0]
+
+        for i in range(1, n):
+            dp[i] = dp[i - 1] + triangle[i][i]
+            for j in range(i - 1, 0, -1):
+                dp[j] = min(dp[j - 1], dp[j]) + triangle[i][j]
+            dp[0] += triangle[i][0]
+
+        return min(dp)
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    print(obj.minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]))  # 11
