cfad54f9d4024dcbfcab5c6aef0f70e5400f839e
code-cold-love <1193898262@qq.com>
2023-04-22 16:12:45 +0800

    1207. 独一无二的出现次数

---
+++ leetcode/problems/unique-number-of-occurrences.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# 1207. 独一无二的出现次数 https://leetcode.cn/problems/unique-number-of-occurrences/
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        counter = Counter(arr)
+        d = dict()
+        for k, v in counter.items():
+            if d.get(v):
+                return False
+            d[v] = k
+        return True
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    print(obj.uniqueOccurrences([1, 2]))  # False
+    print(obj.uniqueOccurrences([1, 2, 2, 1, 1, 3]))  # True
+    print(obj.uniqueOccurrences([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]))  # True
