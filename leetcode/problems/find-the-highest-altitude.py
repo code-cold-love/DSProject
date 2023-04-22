e277364260d24d7fb50e94f98e2171acf1086eda
code-cold-love <1193898262@qq.com>
2023-04-22 15:38:54 +0800

    1732. 找到最高海拔

---
+++ leetcode/problems/find-the-highest-altitude.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# 1732. 找到最高海拔 https://leetcode.cn/problems/find-the-highest-altitude/
+from typing import List
+
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        n = len(gain)
+        ans = max(0, gain[0])
+        for i in range(1, n):
+            gain[i] = gain[i] + gain[i - 1]
+            ans = max(ans, gain[i])
+        return ans
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    print(obj.largestAltitude([-5, 1, 5, 0, -7]))  # 1
