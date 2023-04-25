1e1184adc871ac8dfad853999526660eee456feb
code-cold-love <1193898262@qq.com>
2023-04-25 15:11:40 +0800

    2390. 从字符串中移除星号

---
+++ leetcode/problems/removing-stars-from-a-string.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# 2390. 从字符串中移除星号 https://leetcode.cn/problems/removing-stars-from-a-string/
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stk = []
+        for c in s:
+            if c == '*':
+                stk.pop()
+            else:
+                stk.append(c)
+        return ''.join(stk)
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    print(obj.removeStars('leet**cod*e'))  # lecoe
+    print(obj.removeStars('erase*****'))
