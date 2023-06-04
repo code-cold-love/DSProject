055e397d2d9dd16e63c06694b650951dd30c415e
code-cold-love <1193898262@qq.com>
2023-06-04 16:16:27 +0800

    2465. 不同的平均值数目

---
+++ leetcode/problems/number-of-distinct-averages.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 2465. 不同的平均值数目 https://leetcode.cn/problems/number-of-distinct-averages/
+from typing import List
+
+
+class Solution:
+    def distinctAverages(self, nums: List[int]) -> int:
+        n, avg = len(nums), set()
+        nums.sort()
+        i, j = 0, n - 1
+        while i < j:
+            avg.add((nums[i] + nums[j]) / 2)
+            i += 1
+            j -= 1
+        return len(avg)
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    print(obj.distinctAverages([4, 1, 4, 0, 3, 5]))  # 2
