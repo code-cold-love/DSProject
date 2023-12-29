83cd525452c7fdab8440100c5bc4ecccdaac4c8c
code-cold-love <1193898262@qq.com>
2023-12-29 16:15:24 +0800

    49. 字母异位词分组

---
+++ leetcode/problems/group-anagrams.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# 49. 字母异位词分组 https://leetcode.cn/problems/group-anagrams/
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = defaultdict(list)
+        for st in strs:
+            key = ''.join(sorted(st))
+            mp[key].append(st)
+        return list(mp.values())
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    print(obj.groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']))  # [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
