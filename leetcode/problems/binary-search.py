c0507871e505ae4959f0e326029f4b29948d0d7c
yanwy <yanwy_nh@163.com>
2024-06-25 09:33:38 +0800

    704. 二分查找

--- leetcode/problems/binary-search.py
+++ leetcode/problems/binary-search.py
@@ -6,7 +6,7 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        left, right = 0, len(nums) - 1  # 左闭右闭
         while left <= right:
             mid = left + (right - left) // 2
             if nums[mid] == target:
