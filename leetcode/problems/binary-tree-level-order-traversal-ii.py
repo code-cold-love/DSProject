8be63b5c973b1dfd4fae954825d3a87a7771c32f
yanwy <yanwy_nh@163.com>
2024-07-28 20:25:47 +0800

    107. 二叉树的层序遍历 II

--- leetcode/problems/binary-tree-level-order-traversal-ii.py
+++ leetcode/problems/binary-tree-level-order-traversal-ii.py
@@ -15,28 +15,29 @@ class TreeNode:
 
 class Solution:
     def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
-        ret = list()
+        # 自底向上遍历
+        ans = []
         if not root:
-            return ret
-        q = deque([root])
-        while q:
-            n, tmp = len(q), list()
-            for _ in range(n):
-                node = q.popleft()
-                tmp.append(node.val)
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            ret.append(tmp)
-        return ret[::-1]
+            return ans
+        dq = deque([root])
+        while dq:
+            level = []
+            for _ in range(len(dq)):
+                cur = dq.popleft()
+                level.append(cur.val)
+                if cur.left:
+                    dq.append(cur.left)
+                if cur.right:
+                    dq.append(cur.right)
+            ans.append(level)
+        return ans[::-1]  # 反转
 
 
 if __name__ == '__main__':
-    obj = Solution()
+    solution = Solution()
     head = TreeNode(3)
     head.left = TreeNode(9)
     head.right = TreeNode(20)
     head.right.left = TreeNode(15)
     head.right.right = TreeNode(7)
-    print(obj.levelOrderBottom(head))  # [[15, 7], [9, 20], [3]]
+    print(solution.levelOrderBottom(head))  # [[15, 7], [9, 20], [3]]
