103f45262409244c0c8b7c7493e6734fd7ab1abd
yanwy <yanwy_nh@163.com>
2024-07-30 09:29:52 +0800

    116. 填充每个节点的下一个右侧节点指针

--- leetcode/problems/populating-next-right-pointers-in-each-node.py
+++ leetcode/problems/populating-next-right-pointers-in-each-node.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 116. 填充每个节点的下一个右侧节点指针 https://leetcode.cn/problems/populating-next-right-pointers-in-each-node/
+from collections import deque
 from typing import Optional
 
 
 # Definition for a Node.
 class Node:
-    def __init__(self, val: int = 0, left=None, right=None, _next=None):
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, _next: 'Node' = None):
         self.val = val
         self.left = left
         self.right = right
@@ -14,22 +15,24 @@ class Node:
 
 
 class Solution:
-    def connect(self, root: Optional[Node]) -> Optional[Node]:
-        # 只能使用常量级额外空间
-        # 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度
-        node = root
-        if node is None:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        # 给定一个完美二叉树，所有叶子节点都在同一层，每个父节点有两个子节点
+        # 填充它的每个 next 指针，让这个指针指向其下一个右侧节点
+        if not root:
             return root
-        while node.left:
-            row_start = node
-            row_start.left.next = row_start.right
-            while row_start.next:
-                row_start.right.next = row_start.next.left  # 右节点的 next 为相邻相隔的左节点
-                row_start = row_start.next
-                row_start.left.next = row_start.right
-            node = node.left
+        dq = deque([root])
+        while dq:
+            level_size = len(dq)
+            prev = None
+            for i in range(level_size):
+                curr = dq.popleft()
+                # 在单层遍历时记录本层的头部节点，然后在遍历时让前一个节点指向头部节点
+                if prev:
+                    prev.next = curr
+
+                prev = curr
+                if curr.left:
+                    dq.append(curr.left)
+                if curr.right:
+                    dq.append(curr.right)
         return root
-
-
-if __name__ == '__main__':
-    obj = Solution()
