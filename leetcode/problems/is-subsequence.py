65d7d797448b068e26bafb5938ee0b1e153f3c5f
yanwy <yanwy_nh@163.com>
2024-08-09 13:02:48 +0800

    392. 判断子序列

--- leetcode/problems/is-subsequence.py
+++ leetcode/problems/is-subsequence.py
@@ -3,10 +3,6 @@
 # 392. 判断子序列 https://leetcode.cn/problems/is-subsequence/
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if not s:
-            return True
-        elif s and not t:
-            return False
         s_len, t_len = len(s), len(t)
         if s_len > t_len:
             return False
@@ -14,12 +10,8 @@ class Solution:
         while i < s_len and j < t_len:
             if s[i] == t[j]:
                 i += 1
-                j += 1
-            else:
-                j += 1
-        if i == s_len:
-            return True
-        return False
+            j += 1
+        return i == s_len
 
 
 if __name__ == '__main__':
