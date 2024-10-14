e7d60f9c4619b893b2192ff4b0c02ab81b3c1ed2
yanwy <yanwy_nh@163.com>
2024-10-14 09:34:17 +0800

    77. 组合：重写 combine 函数以使用回溯法生成组合
    将原有的 combine 函数实现改为使用回溯法，而不是依赖于标准库中的 combinations 函数。

--- leetcode/problems/combinations.py
+++ leetcode/problems/combinations.py
@@ -2,19 +2,28 @@
 # -*- coding: utf-8 -*-
 # 77. 组合 https://leetcode.cn/problems/combinations/
 from typing import List
-from itertools import combinations
 
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        if n == 1:
-            return [[1]]
-        elif k == 1:
-            return [[i] for i in range(1, n + 1)]
-        return list(combinations(range(1, n + 1), k))
+        # 返回范围 [1, n] 中所有可能的 k 个数的组合
+        # 1 <= k <= n
+        ans, path = [], []
+
+        def backtrack(n: int, k: int, start: int):
+            if len(path) == k:  # 终止条件
+                ans.append(path[:])
+                return
+            for i in range(start, n + 1):  # 选择本层集合中元素
+                path.append(i)  # 处理节点
+                backtrack(n, k, i + 1)  # 递归
+                path.pop()  # 回溯，撤销处理的节点
+
+        backtrack(n, k, 1)
+        return ans
 
 
 if __name__ == '__main__':
     obj = Solution()
     print(obj.combine(1, 1))  # [[1]]
-    print(obj.combine(4, 2))
+    print(obj.combine(4, 2))  # [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
