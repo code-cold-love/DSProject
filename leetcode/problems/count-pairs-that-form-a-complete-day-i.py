870a154a9c6a06df7cdd3870f2a37b42a685113b
yanwy <yanwy_nh@163.com>
2024-10-22 09:08:25 +0800

    3184. 构成整天的下标对数目 I

---
+++ leetcode/problems/count-pairs-that-form-a-complete-day-i.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 3184. 构成整天的下标对数目 I https://leetcode.cn/problems/count-pairs-that-form-a-complete-day-i/
+from typing import List
+
+
+class Solution:
+    def countCompleteDayPairs(self, hours: List[int]) -> int:
+        # 遍历 hours 的同时，用一个哈希表记录元素的出现次数
+        # 对于 hours[i]，需要知道左边有多少个模 24 是 24 - hours[i] % 24 的数
+        H = 24
+        ans = 0
+        cnt = [0] * H  # 维护 hours[i] % 24 的出现次数
+        for h in hours:
+            # (H - h % H) % H 最后多进行了一次取模运算
+            # 是因为如果 hours[i] % 24 是 0，那么需要知道左边有多少个模 24 也是 0 的数
+            ans += cnt[(H - h % H) % H]
+            cnt[h % H] += 1
+        return ans
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.countCompleteDayPairs([12, 12, 30, 24, 24]))  # 2
+    print(solution.countCompleteDayPairs([72, 48, 24, 3]))  # 3
